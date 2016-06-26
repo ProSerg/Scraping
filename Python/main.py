64b04f82222a94d0d4272b88145df7ffca053345
ProSerg <primatvko@gmail.com>
2016-06-26 23:24:01 +0300

    first step with Scraping

---
+++ Python/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+# working with network
+import urllib.request
+# parsing HTML
+from bs4 import BeautifulSoup
+
+def get_html(url):
+	response = urllib.request.urlopen(url)
+	return response.read()
+
+def main():
+	print("Hello,World!{}",get_html("http://yandex.ru"))
+	pass
+	
+if  __name__ == "__main__":
+	main()
+
+
